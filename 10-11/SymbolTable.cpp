jomiller/nand2tetris

commit 7b9a75c823567cc157a994f431ffa6070d60e4ab

Fix MSVC 2017 build errors and warnings

10-11/SymbolTable.cpp
@@ -27,37 +27,39 @@
 #include <algorithm>
 #include <cassert>
 
-n2t::SymbolTable::SymbolTable() : m_nextIndices()
+n2t::SymbolTable::SymbolTable() : m_nextVarIndices()
 {
 }
 
 void n2t::SymbolTable::startSubroutine()
 {
     m_subroutineTable.clear();
-    m_nextIndices[JackUtil::toUnderType(VariableKind::Argument)] = 0;
-    m_nextIndices[JackUtil::toUnderType(VariableKind::Local)]    = 0;
+    m_nextVarIndices[JackUtil::toUnderType(VariableKind::Argument)] = 0;
+    m_nextVarIndices[JackUtil::toUnderType(VariableKind::Local)]    = 0;
 }
 
 void n2t::SymbolTable::define(const std::string& name, const std::string& type, VariableKind kind)
 {
     HashTable&     table = getHashTable(kind);
-    HashTableEntry entry(type, kind, getNextIndex(kind));
+    HashTableEntry entry(type, kind, getNextVarIndex(kind));
     JACK_THROW_COND(table.emplace(name, std::move(entry)).second,
                     "Identifier with name (" + name + ") already defined in the current scope");
 }
 
-unsigned int n2t::SymbolTable::varCount(VariableKind kind) const
+int16_t n2t::SymbolTable::varCount(VariableKind kind) const
 {
     const HashTable& table = getHashTable(kind);
 
     // clang-format off
-    return std::count_if(table.begin(),
-                         table.end(),
-                         [kind](const auto& entry)
-                         {
-                             return (entry.second.kind == kind);
-                         });
+    const auto count = std::count_if(table.begin(),
+                                     table.end(),
+                                     [kind](const auto& entry)
+                                     {
+                                         return (entry.second.kind == kind);
+                                     });
     // clang-format on
+
+    return static_cast<int16_t>(count);
 }
 
 const std::string& n2t::SymbolTable::typeOf(const std::string& name) const
@@ -74,7 +76,7 @@ n2t::VariableKind n2t::SymbolTable::kindOf(const std::string& name) const
     return entry.kind;
 }
 
-unsigned int n2t::SymbolTable::indexOf(const std::string& name) const
+int16_t n2t::SymbolTable::indexOf(const std::string& name) const
 {
     const HashTableEntry& entry = getHashTableEntry(name);
     JACK_THROW_COND(entry.kind != VariableKind::None, "Identifier (" + name + ") not defined in the current scope");
@@ -118,8 +120,16 @@ const n2t::SymbolTable::HashTableEntry& n2t::SymbolTable::getHashTableEntry(cons
     return none;
 }
 
-unsigned int n2t::SymbolTable::getNextIndex(VariableKind kind)
+int16_t n2t::SymbolTable::getNextVarIndex(VariableKind kind)
 {
     assert(kind < VariableKind::Count);
-    return m_nextIndices[JackUtil::toUnderType(kind)]++;
+
+    const int16_t maxVarIndex  = std::numeric_limits<int16_t>::max();
+    int16_t*      nextVarIndex = &m_nextVarIndices[JackUtil::toUnderType(kind)];
+
+    JACK_THROW_COND(*nextVarIndex < maxVarIndex,
+                    "Variable count for kind (" + JackUtil::toString(kind) + ") exceeds the limit (" +
+                        std::to_string(maxVarIndex) + ")");
+
+    return *nextVarIndex++;
 }

06/AssemblyEngine.cpp
@@ -35,6 +35,7 @@
 #include <bitset>
 #include <cstdint>
 #include <fstream>
+#include <limits>
 #include <locale>
 #include <utility>
 
@@ -72,6 +73,8 @@ void n2t::AssemblyEngine::buildSymbolTable()
 
     try
     {
+        const int16_t maxRomAddress = std::numeric_limits<int16_t>::max();
+
         // ROM address to advance when a C-instruction or an A-instruction is encountered,
         // but does not change when a label pseudocommand or a comment is encountered
         int16_t nextRomAddress = 0;
@@ -81,6 +84,8 @@ void n2t::AssemblyEngine::buildSymbolTable()
             const CommandType commandType = symbolParser.commandType();
             if ((commandType == CommandType::A) || (commandType == CommandType::C))
             {
+                ASM_THROW_COND(nextRomAddress < maxRomAddress,
+                               "Instruction count exceeds the limit (" + std::to_string(maxRomAddress) + ")");
                 ++nextRomAddress;
             }
             else if (commandType == CommandType::L)
@@ -110,6 +115,8 @@ void n2t::AssemblyEngine::generateCode()
 
     try
     {
+        const int16_t maxRamAddress = std::numeric_limits<int16_t>::max();
+
         // starting RAM memory address for mapped variables
         int16_t nextRamAddress = 0x0010;
 
@@ -157,6 +164,9 @@ void n2t::AssemblyEngine::generateCode()
                         // associate the variable with the next available RAM address
                         m_symbolTable.addEntry(symbol, nextRamAddress);
                         targetAddress = nextRamAddress;
+
+                        ASM_THROW_COND(nextRamAddress < maxRamAddress,
+                                       "Symbol count exceeds the limit (" + std::to_string(maxRamAddress) + ")");
                         ++nextRamAddress;
                     }
                 }

06/Code.cpp
@@ -74,34 +74,34 @@ uint16_t n2t::Code::comp(const std::string& comp)
     // clang-format off
     static const std::map<std::string_view, uint16_t> compCodes =
     {
-        {"0",   0b0101010},
-        {"1",   0b0111111},
-        {"-1",  0b0111010},
-        {"D",   0b0001100},
-        {"A",   0b0110000},
-        {"M",   0b1110000},
-        {"!D",  0b0001101},
-        {"!A",  0b0110001},
-        {"!M",  0b1110001},
-        {"-D",  0b0001111},
-        {"-A",  0b0110011},
-        {"-M",  0b1110011},
-        {"D+1", 0b0011111},
-        {"A+1", 0b0110111},
-        {"M+1", 0b1110111},
-        {"D-1", 0b0001110},
-        {"A-1", 0b0110010},
-        {"M-1", 0b1110010},
-        {"D+A", 0b0000010},
-        {"D+M", 0b1000010},
-        {"D-A", 0b0010011},
-        {"D-M", 0b1010011},
-        {"A-D", 0b0000111},
-        {"M-D", 0b1000111},
-        {"D&A", 0b0000000},
-        {"D&M", 0b1000000},
-        {"D|A", 0b0010101},
-        {"D|M", 0b1010101}
+        {"0",   uint16_t{0b0101010}},
+        {"1",   uint16_t{0b0111111}},
+        {"-1",  uint16_t{0b0111010}},
+        {"D",   uint16_t{0b0001100}},
+        {"A",   uint16_t{0b0110000}},
+        {"M",   uint16_t{0b1110000}},
+        {"!D",  uint16_t{0b0001101}},
+        {"!A",  uint16_t{0b0110001}},
+        {"!M",  uint16_t{0b1110001}},
+        {"-D",  uint16_t{0b0001111}},
+        {"-A",  uint16_t{0b0110011}},
+        {"-M",  uint16_t{0b1110011}},
+        {"D+1", uint16_t{0b0011111}},
+        {"A+1", uint16_t{0b0110111}},
+        {"M+1", uint16_t{0b1110111}},
+        {"D-1", uint16_t{0b0001110}},
+        {"A-1", uint16_t{0b0110010}},
+        {"M-1", uint16_t{0b1110010}},
+        {"D+A", uint16_t{0b0000010}},
+        {"D+M", uint16_t{0b1000010}},
+        {"D-A", uint16_t{0b0010011}},
+        {"D-M", uint16_t{0b1010011}},
+        {"A-D", uint16_t{0b0000111}},
+        {"M-D", uint16_t{0b1000111}},
+        {"D&A", uint16_t{0b0000000}},
+        {"D&M", uint16_t{0b1000000}},
+        {"D|A", uint16_t{0b0010101}},
+        {"D|M", uint16_t{0b1010101}}
     };
     // clang-format on
 

07-08/CodeWriter.cpp
@@ -196,7 +196,7 @@ void n2t::CodeWriter::writePushPop(CommandType command, const std::string& segme
         case SegmentType::Argument:
             if (!m_currentFunction.name.empty())
             {
-                m_currentFunction.numParameters = std::max<unsigned int>(m_currentFunction.numParameters, index + 1);
+                m_currentFunction.numParameters = std::max<int16_t>(m_currentFunction.numParameters, index + 1);
             }
             break;
 
@@ -310,9 +310,11 @@ void n2t::CodeWriter::writeIf(const std::string& label)
     // clang-format on
 }
 
-void n2t::CodeWriter::writeFunction(const std::string& functionName, unsigned int numLocals)
+void n2t::CodeWriter::writeFunction(const std::string& functionName, int16_t numLocals)
 {
     validateFunction();
+
+    VM_THROW_COND(numLocals >= 0, "Number of function local variables (" + std::to_string(numLocals) + ") is negative");
 
     VM_THROW_COND(!std::isdigit(functionName.front(), std::locale()),
                   "Function name (" + functionName + ") begins with a digit");
@@ -331,7 +333,7 @@ void n2t::CodeWriter::writeFunction(const std::string& functionName, unsigned in
     {
         m_file << "D=0\n";
     }
-    for (unsigned int lcl = 0; lcl < numLocals; ++lcl)
+    for (int16_t lcl = 0; lcl < numLocals; ++lcl)
     {
         pushFromD();
     }
@@ -406,8 +408,10 @@ void n2t::CodeWriter::writeReturn()
     // clang-format on
 }
 
-void n2t::CodeWriter::writeCall(const std::string& functionName, unsigned int numArguments)
+void n2t::CodeWriter::writeCall(const std::string& functionName, int16_t numArguments)
 {
+    VM_THROW_COND(numArguments >= 0, "Number of function arguments (" + std::to_string(numArguments) + ") is negative");
+
     m_calledFunctions.emplace(functionName, numArguments);
     const std::string label = makeLabel("RETURN" + std::to_string(getNextLabelId()));
 

07-08/CodeWriter.h
@@ -75,13 +75,13 @@ public:
     void writeIf(const std::string& label);
 
     // Writes assembly code that effects the function command.
-    void writeFunction(const std::string& functionName, unsigned int numLocals);
+    void writeFunction(const std::string& functionName, int16_t numLocals);
 
     // Writes assembly code that effects the return command.
     void writeReturn();
 
     // Writes assembly code that effects the call command.
-    void writeCall(const std::string& functionName, unsigned int numArguments);
+    void writeCall(const std::string& functionName, int16_t numArguments);
 
     // Closes the output file.
     void close();
@@ -95,24 +95,24 @@ public:
 private:
     struct FunctionInfo
     {
-        std::string  name;
-        unsigned int numParameters = 0;
+        std::string name;
+        int16_t     numParameters = 0;
 
         FunctionInfo() = default;
 
-        FunctionInfo(std::string n, unsigned int p) : name(std::move(n)), numParameters(p)
+        FunctionInfo(std::string n, int16_t p) : name(std::move(n)), numParameters(p)
         {
         }
     };
 
     struct FunctionCallInfo
     {
-        std::string  name;
-        unsigned int numArguments = 0;
+        std::string name;
+        int16_t     numArguments = 0;
 
         FunctionCallInfo() = default;
 
-        FunctionCallInfo(std::string n, unsigned int a) : name(std::move(n)), numArguments(a)
+        FunctionCallInfo(std::string n, int16_t a) : name(std::move(n)), numArguments(a)
         {
         }
 
@@ -133,16 +133,16 @@ private:
     void         validateFunction();
     void         validateFunctionCalls() const;
 
-    std::filesystem::path               m_outputFilename;
-    std::ofstream                       m_file;
-    std::string                         m_currentInputFilename;
-    FunctionInfo                        m_currentFunction;
-    std::set<std::string>               m_labels;
-    std::set<std::string>               m_gotoDestinations;
-    std::map<std::string, unsigned int> m_definedFunctions;
-    std::set<FunctionCallInfo>          m_calledFunctions;
-    unsigned int                        m_nextLabelId = 0;
-    bool                                m_closed      = false;
+    std::filesystem::path          m_outputFilename;
+    std::ofstream                  m_file;
+    std::string                    m_currentInputFilename;
+    FunctionInfo                   m_currentFunction;
+    std::set<std::string>          m_labels;
+    std::set<std::string>          m_gotoDestinations;
+    std::map<std::string, int16_t> m_definedFunctions;
+    std::set<FunctionCallInfo>     m_calledFunctions;
+    unsigned int                   m_nextLabelId = 0;
+    bool                           m_closed      = false;
 };
 }  // namespace n2t
 

10-11/CompilationEngine.cpp
@@ -459,7 +459,7 @@ void n2t::CompilationEngine::compileTerm()
     else if (tokenType == TokenType::StringConst)
     {
         const std::string stringConst = compileStringConstant();
-        m_vmWriter.writePush(SegmentType::Constant, stringConst.length());
+        m_vmWriter.writePush(SegmentType::Constant, static_cast<int16_t>(stringConst.length()));
         m_vmWriter.writeCall("String.new", 1);
         for (auto c : stringConst)
         {
@@ -671,7 +671,7 @@ void n2t::CompilationEngine::compileSubroutineBody()
     if (m_currentSubroutine.type == Keyword::Constructor)
     {
         // allocate at least one word so that objects of field-less classes will still have valid addresses
-        const unsigned int numFields = std::max(m_symbolTable.varCount(VariableKind::Field), 1u);
+        const int16_t numFields = std::max(m_symbolTable.varCount(VariableKind::Field), int16_t{1});
         m_vmWriter.writePush(SegmentType::Constant, numFields);
         m_vmWriter.writeCall("Memory.alloc", 1);
 
@@ -758,11 +758,12 @@ void n2t::CompilationEngine::compileSubroutineCall(const std::string& identifier
     compileSymbol('(');
     compileExpressionList();
     compileSymbol(')');
-    subroutineCall.numArguments = m_argumentCounts.top();
+    const int16_t currentArgumentCount = m_argumentCounts.top();
+    subroutineCall.numArguments        = currentArgumentCount;
 
     // the number of arguments is determined from the number of expressions in the expression list
     m_vmWriter.writeCall(classObjectName + "." + subroutineName,
-                         m_argumentCounts.top() + static_cast<unsigned int>(thisArgument));
+                         currentArgumentCount + static_cast<int16_t>(thisArgument));
 
     if (classObjectName == m_className)
     {
@@ -773,10 +774,10 @@ void n2t::CompilationEngine::compileSubroutineCall(const std::string& identifier
 
 void n2t::CompilationEngine::validateSubroutineCalls() const
 {
-    const auto sub = m_definedSubroutines.find("main");
-    JACK_THROW_COND(
-        (m_className != "Main") || ((sub != m_definedSubroutines.end()) && (sub->second.type == Keyword::Function)),
-        "Class does not contain a function named 'main'");
+    const auto mainSub = m_definedSubroutines.find("main");
+    JACK_THROW_COND((m_className != "Main") ||
+                        ((mainSub != m_definedSubroutines.end()) && (mainSub->second.type == Keyword::Function)),
+                    "Class does not contain a function named 'main'");
 
     for (const SubroutineCallInfo& call : m_calledSubroutines)
     {

10-11/CompilationEngine.h
@@ -55,14 +55,14 @@ public:
 private:
     struct SubroutineInfo
     {
-        Keyword      type   = Keyword::Constructor;
-        bool         isVoid = false;
-        std::string  name;
-        unsigned int numParameters = 0;
+        Keyword     type   = Keyword::Constructor;
+        bool        isVoid = false;
+        std::string name;
+        int16_t     numParameters = 0;
 
         SubroutineInfo() = default;
 
-        SubroutineInfo(Keyword t, bool v, std::string n, unsigned int p) :
+        SubroutineInfo(Keyword t, bool v, std::string n, int16_t p) :
             type(t),
             isVoid(v),
             name(std::move(n)),
@@ -73,14 +73,14 @@ private:
 
     struct SubroutineCallInfo
     {
-        Keyword      type = Keyword::Function;
-        std::string  name;
-        unsigned int numArguments = 0;
-        bool         expression   = false;
+        Keyword     type = Keyword::Function;
+        std::string name;
+        int16_t     numArguments = 0;
+        bool        expression   = false;
 
         SubroutineCallInfo() = default;
 
-        SubroutineCallInfo(Keyword t, std::string n, unsigned int a, bool e) :
+        SubroutineCallInfo(Keyword t, std::string n, int16_t a, bool e) :
             type(t),
             name(std::move(n)),
             numArguments(a),
@@ -180,7 +180,7 @@ private:
     SubroutineInfo                        m_currentSubroutine;
     std::map<std::string, SubroutineInfo> m_definedSubroutines;
     std::set<SubroutineCallInfo>          m_calledSubroutines;
-    std::stack<unsigned int>              m_argumentCounts;
+    std::stack<int16_t>                   m_argumentCounts;
     bool                                  m_inReturnStatement = false;
     unsigned int                          m_nextLabelId       = 0;
 };

10-11/JackTokenizer.cpp
@@ -50,6 +50,11 @@ void n2t::JackTokenizer::TokenizerFunction::reset()
     m_lineCount = 0;
 }
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4706)  // assignment within conditional expression
+#endif
+
 bool n2t::JackTokenizer::TokenizerFunction::operator()(std::istreambuf_iterator<char>& next,
                                                        std::istreambuf_iterator<char>  end,
                                                        std::string&                    tok)
@@ -162,6 +167,10 @@ bool n2t::JackTokenizer::TokenizerFunction::operator()(std::istreambuf_iterator<
     }
     return true;
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 bool n2t::JackTokenizer::TokenizerFunction::isDelimiter(char c)
 {

commit f235623b83f4e1b1ebe7fc6bd1d11094e801da3a

Fix regression introduced in 7b9a75c823567cc157a994f431ffa6070d60e4ab

10-11/SymbolTable.cpp
@@ -132,5 +132,5 @@ int16_t n2t::SymbolTable::getNextVarIndex(VariableKind kind)
                     "Variable count for kind (" + JackUtil::toString(kind) + ") exceeds the limit (" +
                         std::to_string(maxVarIndex + 1) + ")");
 
-    return *nextVarIndex++;
+    return (*nextVarIndex)++;
 }